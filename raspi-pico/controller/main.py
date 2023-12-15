c14000dd44c9d1cb9287cf982bda2809c732552d
Robert Gray <rgray@ttsa.net>
2023-12-14 20:22:51 -0800

    main.py using onboard bluetooth

--- raspi-pico/controller/main.py
+++ raspi-pico/controller/main.py
@@ -1,46 +1,87 @@
-import secrets
-import network
+#test.py
+import bluetooth
+from ble_central import BLESimpleCentral
 import time
 import urequests
 import json
-import machine
+import secrets
+from wifi import WifiConnector
+from lionchief import LionChief
 from bike import run_bike
-#from machine import Pin, PWM, Timer
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-server_ip = secrets.IP
-port = secrets.PORT
-url = server_ip + port + '/apis/status/'
-headers = {'Content-Type': 'application/json'}
-led = machine.Pin("LED", machine.Pin.OUT)
-while not wlan.isconnected():
-    led.high()
-    wlan.connect(secrets.SSID, secrets.PASSWORD)
-    time.sleep(1)  
-    led.low()
-    time.sleep(3)
-while wlan.isconnected():
-    try:
-        print(wlan.isconnected())
-        print(url)
-        message = {
-            'train': secrets.TRAIN_ID,
-            'status': 'POWERED',
-            }
-        m = json.dumps(message)
-        h = json.dumps(headers)
-        print(m)
-        led = machine.Pin("LED", machine.Pin.OUT)
-        led.high()
-        r = urequests.post(url, data=m, headers=headers, timeout=1.0)
-        print(r.status_code)
-        if r.status_code == 201:
-            
-            led.low()
-            run_bike()
+if __name__ == "__main__":
+    addr_string = 'FA:28:35:37:B6:54'
+    addr = bytes.fromhex(addr_string.replace(':', ''))
+    print(addr)
+    addr_type = 0
+
+    #ble = bluetooth.BLE()
+    chief = LionChief(addr)
+    server_ip = secrets.IP
+    port = secrets.PORT
+    url = server_ip + port + '/apis/status/'
+    headers = {'Content-Type': 'application/json'}
+    wifi = WifiConnector(secrets.SSID, secrets.PASSWORD)
+    time.sleep(1)
+    print(secrets.SSID, ' ', secrets.PASSWORD)
+    print("Wifi ", wifi.isconnected())
+    bike_running = False
+    while(1):
+        not_found = False
+        while not chief.is_connected():
+            time.sleep_ms(1000)
+            print("We are not connected")
             break
+        
+        def on_rx(v):
+            buf1 = bytearray(v)
+            print("RX ")
+            for _ in v:
+                print(chr(_), end='')
+            print("")
+
+        chief._ble_central.on_notify(on_rx)
+        
+        while chief.is_connected():
+            print("We are connected to train")
+            with_response=False
+            try:
+                #v = str(i) + "_"
+                
+                chief.set_horn(True)
+                time.sleep(.5)
+                
+                chief.set_speed(25)
+                time.sleep(2)
+                chief.set_horn(False)
+                
+            except:
+                print("TX failed")
+            try:
+                if not wifi.isconnected() :
+                    wifi.connect()
+                else:
+                    message = {
+                        'train': 2,
+                        'status': 'RUNNING',
+                        }
+                    m = json.dumps(message)
+                    h = json.dumps(headers)
+                    print(m)
+                    r = urequests.post(url, data=m, headers=headers)
+                    print("Hey we just sent a request")
+                    print(r.status_code)
+            except Exception as e:
+                print(e)
             
-    except Exception as e:
-        print(e)
-
-
+            time.sleep_ms(2000 if with_response else 2000)
+            run_bike()
+                
+        chief.scan_connect()
+           
+            
+        print("Disconnected")
+        print(" Is the bike running?", bike_running)
+        print("Wifi Connected ", wifi.isconnected())
+    
+        
+        
